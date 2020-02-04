cb2b5cb034e1511c6af874d0fb2aac1955ad68b6
BliznyukS <bliznyuk.sergey@gmail.com>
2020-02-04 16:27:20 +0200

    Java For Dummies - chapter01. first commit.

---
+++ src/chapter01/PayBarry.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class PayBarry {
+    public static void main(String[] args) {
+        double checkAmount = 1257.63;
+        System.out.print("Оплатить заказ Бари Берда, ");
+        System.out.print("сумма : ");
+        System.out.print(checkAmount);
+        System.out.print(" рублей");
+    }
+}
